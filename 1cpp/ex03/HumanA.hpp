6a21e1e500c617f56bf69168f9870fffac5c3ed5
yabi <hin050924@gmail.com>
2025-01-30 00:48:00 +0800

    ex03 inprogress

---
+++ 1cpp/ex03/HumanA.hpp
@@ -0,0 +1,25 @@
+#ifndef HUMANA_HPP
+# define HUMANA_HPP
+
+#include <string>
+#include <iostream>
+
+class HumanA
+{
+    private:
+    /* data */
+    public:
+    HumanA(/* args */);
+    ~HumanA();
+};
+
+HumanA::HumanA(/* args */)
+{
+}
+
+HumanA::~HumanA()
+{
+}
+
+
+#endif
---
+++ 1cpp/ex03/HumanB.hpp
@@ -0,0 +1,26 @@
+#ifndef HUMANB_HPP
+# define HUMANB_HPP
+
+#include <string>
+#include <iostream>
+
+class HumanB
+{
+    private:
+    /* data */
+    public:
+    HumanB(/* args */);
+    ~HumanB();
+};
+
+HumanB::HumanB(/* args */)
+{
+}
+
+HumanB::~HumanB()
+{
+}
+
+
+
+#endif
