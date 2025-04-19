ecd97673ca56450b4afb79f9ce1323b4f6626efc
yabi <hin050924@gmail.com>
2025-04-19 21:47:24 +0800

    cpp06 ex01 & ex02

---
+++ 6cpp/ex02/ABC.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+# include <iostream>
+# include <cstdlib>
+# include <ctime>
+
+# ifndef MYFLAG
+#  define MYFLAG 0
+# endif
+
+using std::cout;
+using std::endl;
+
+class Base
+{
+public:
+    virtual ~Base() {};
+};
+
+class A: public Base
+{
+};
+
+class B: public Base
+{
+};
+
+class C: public Base
+{};
---
+++ 6cpp/ex02/main.cpp
@@ -0,0 +1,115 @@
+#include "ABC.hpp"
+
+Base *generate()
+{
+    cout << "generate: ";
+    srand(time(NULL));
+
+    unsigned int n = rand();
+    if (n % 3 == 1)
+    {
+        cout << "generated A" << endl;
+        return (new A);
+    }
+    else if (n % 3 == 2)
+    {    
+        cout << "generated B" << endl;
+        return (new B);
+    }
+    cout << "generated C" << endl;
+
+    return (new C);
+}
+
+void identify(Base *p)
+{
+    cout << "pointer indentify: ";
+    A *a = dynamic_cast<A *>(p);
+    if (a)
+    {
+        cout << "p is A" << endl;
+        return ;
+    }
+    B *b = dynamic_cast<B *>(p);
+    if (b)
+    {
+        cout << "p is B" << endl;
+        return ;
+    }
+    C *c = dynamic_cast<C *>(p);
+    if (c)
+    {
+        cout << "p is C" << endl;
+        return ;
+    }
+    cout << "?" << endl;
+}
+
+void identify(Base &p)
+{
+    cout << "reference indentify: ";
+    try
+    {
+        A &a = dynamic_cast<A &>(p);
+        (void)a;
+        cout << "p is A" << endl;
+        return ;
+    }
+    catch(const std::exception& e)
+    {
+        (void)e;
+    }
+    try
+    {
+        B &b = dynamic_cast<B &>(p);
+        (void)b;
+        cout << "p is B" << endl;
+        return ;
+    }
+    catch(const std::exception& e)
+    {
+        (void)e;
+    }
+    try
+    {
+        C &c = dynamic_cast<C &>(p);
+        (void)c;
+        cout << "p is C" << endl;
+        return ;
+    }
+    catch(const std::exception& e)
+    {
+        (void)e;
+    }
+    cout << "?" << endl;
+}
+
+void testBase()
+{
+    cout << "----test base----" << endl;
+    Base *base = new Base;
+
+    identify(base);
+    identify(*base);
+    delete base;
+}
+
+void test()
+{
+    cout << "----normal test----" << endl;
+    Base *ran = generate();
+
+    identify(ran);
+    identify(*ran);
+
+    delete ran;
+}
+
+int main()
+{
+    if (MYFLAG == 0)
+        test();
+    if (MYFLAG == 1)
+        testBase();
+    return (0);
+}
