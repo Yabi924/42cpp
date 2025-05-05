5ab023370d4d9f32dc3a0eccedf7cc524c4fb025
yabi <hin050924@gmail.com>
2025-05-05 23:29:35 +0800

    cpp09 ex00 setup

---
+++ 9cpp/ex00/BitcoinExchange.cpp
@@ -0,0 +1,41 @@
+#include "BitcoinExchange.hpp"
+
+bool validDate(string line)
+{
+
+}
+bool validValue(string line)
+{
+
+}
+
+bool checking(string infile)
+{
+    if (validDate(infile))
+        return (true);
+    else if (validValue(infile))
+        return (true);
+    return (false);
+}
+
+int main(int argc, char **argv)
+{
+    if (argc != 2)
+    {
+        cerr << "Error, follow: ./btc filename" << endl;
+        return (1);
+    }
+
+    std::ifstream infile(argv[1]);
+    if (!infile)
+    {
+        cerr << "Can't open " << argv[1] << "." << endl;
+        return (1);
+    }
+
+    string file;
+    while (std::getline(infile, file))
+        if (checking(file))
+            return (1);
+    
+}
---
+++ 9cpp/ex00/BitcoinExchange.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <string>
+#include <iostream>
+#include <fstream>
+
+using std::string;
+using std::cout;
+using std::endl;
+using std::cerr;
+
+bool validDate(string line);
+bool validValue(string line);
